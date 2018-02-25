aced427c655de6fbaf6228825826d8cc2044980d
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-02-25 11:57:58 -0700

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("/public"));
+app.use(express.static("public"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 //Override with POSt having ?)method =delete
