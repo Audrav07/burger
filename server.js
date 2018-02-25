7bf72a482de3c1b4ffb1076b172bd2ac0f70fcb4
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-02-25 14:52:17 -0700

    update

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
+
+ app.use(express.static(path.join(__dirname, 'public')));
+
 // app.use(express.static("/public"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,8 +23,8 @@ app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(process.cwd() + '/public'));
+
+// app.use(express.static(process.cwd() + '/public'));
 // app.use(express.static(path.join(__dirname + 'assets')));
 
 app.use("/", routes);
