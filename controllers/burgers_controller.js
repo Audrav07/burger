d71d7842dfd5ca228590bc9ed7092e7aa91d5683
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-02-26 12:49:46 -0700

    pushing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,39 +6,39 @@ const router = express.Router();
 
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res){
-  burger.all(function(data){
-    var hbsObject = {
-      burger: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
-
-
-// router.get("/", (req, res) => {
-//   burger.all((data) => {
-//     let hbsObject = {
+// router.get("/", function(req, res){
+//   burger.all(function(data){
+//     var hbsObject = {
 //       burger: data
 //     };
-  
+//     console.log(hbsObject);
 //     res.render("index", hbsObject);
 //   });
 // });
+
+
+router.get("/", (req, res) => {
+  burger.all((data) => {
+    let hbsObject = {
+      burger: data
+    };
+  
+    res.render("index", hbsObject);
+  });
+});
 
 // router.post("/", function(req, res){
 //   burger.insert("burger_name", )
 // })
 
-router.post("/api/burger", (req, res) => {
+router.post("/api/", (req, res) => {
   burger.insert("burger_name", req.body.burger_name, function()
     {
       res.redirect("/");
     });
 });
 
-router.put("/api/burger/:id", (req, res) =>{
+router.put("/burger/:id", (req, res) =>{
 
 //id of button
 let condition = "id = " + req.params.id;
