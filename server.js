15b4f7968792afe89a60192c7d83581e73c9ad17
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-02-25 23:05:18 -0700

    make it better

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const PORT = process.env.PORT || 3000;
 // app.use(express.static("/public"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 //Override with POSt having ?)method =delete
 app.use(methodOverride("_method"));
 
