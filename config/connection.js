c82d0371b67bb0c870ba5fca8496e4fa1066a8e6
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-02-25 21:42:16 -0700

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,133 @@
-// Set up MySQL connection.
-const mysql = require("mysql");
+// // Set up MySQL connection.
+// const mysql = require("mysql");
 
-var connection;
+// var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
 
-  // DB is local on localhost
-  var connection = mysql.createConnection({
-    // port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-  });
+//   // DB is local on localhost
+//   var connection = mysql.createConnection({
+//     // port: 3306,
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'burgers_db'
+//   });
+// }
+
+// // Make connection.
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+
+try{
+    var mysql_npm = require('../node_modules/mysql');
+}catch(err){
+    console.log("Cannot find `mysql` module. Is it installed ? Try `npm install mysql` or `npm install`.");
 }
 
-// Make connection.
+//-
+//- Connection configuration
+//-
+var db_config = {
+    host         : 'localhost',
+    user         : 'root',
+    password     : '',
+    database     : 'burgers_db'
+};
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+
+//-
+//- Create the connection variable
+//-
+var connection = mysql_npm.createPool(db_config);
+
+
+//-
+//- Establish a new connection
+//-
+connection.getConnection(function(err){
+    if(err) {
+        // mysqlErrorHandling(connection, err);
+        console.log("\n\t *** Cannot establish a connection with the database. ***");
+
+        connection = reconnect(connection);
+    }else {
+        console.log("\n\t *** New connection established with the database. ***")
+    }
 });
 
 
-// Export connection for our ORM to use.
+//-
+//- Reconnection function
+//-
+function reconnect(connection){
+    console.log("\n New connection tentative...");
+
+    //- Create a new one
+    connection = mysql_npm.createPool(db_config);
+
+    //- Try to reconnect
+    connection.getConnection(function(err){
+        if(err) {
+            //- Try to connect every 2 seconds.
+            setTimeout(reconnect(connection), 2000);
+        }else {
+            console.log("\n\t *** New connection established with the database. ***")
+            return connection;
+        }
+    });
+}
+
+
+//-
+//- Error listener
+//-
+connection.on('error', function(err) {
+
+    //-
+    //- The server close the connection.
+    //-
+    if(err.code === "PROTOCOL_CONNECTION_LOST"){    
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        return reconnect(connection);
+    }
+
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        return reconnect(connection);
+    }
+
+    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        return reconnect(connection);
+    }
+
+    else if(err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE"){
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+    }
+
+    else{
+        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+        return reconnect(connection);
+    }
+
+});
+
+
+//-
+//- Export
+//-
 module.exports = connection;
